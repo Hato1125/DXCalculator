79ab112ecbc35b3988416ff0c77a5e1220dc421a
Hato1125 <mepur@LAPTOP-3QNMCQ12>
2022-10-12 00:53:18 +0900

    Buttonを表示するクラスのコードを整理

--- DXCalculator/Screens/Buttons/BtnScreen.cs
+++ DXCalculator/Screens/Buttons/BtnScreen.cs
@@ -1,72 +1,143 @@
 ﻿using DxLibDLL;
 using DXCalculator.GUI;
 using ABI.Microsoft.UI.Xaml.Controls;
+using System.Diagnostics;
 
 namespace DXCalculator;
 
 internal class BtnScreen
 {
-    private const float Padding = 5;
+    private const int Padding = 3;
+
     private GUI.Panel? panel = null;
-    private GUI.Button[]? button = null;
-    private string[] texts = { "7", "8", "9", "4", "5", "6", "1", "2", "3" };
+    private Dictionary<string, GUI.Button>? Buttons = null;
+    private string[] texts =
+    {
+        "0", "1", "2", "3", "4", "5",
+        "6", "7", "8", "9", "+", "-",
+        "x", "÷", "%", "=", ".", "+/-"
+    };
 
     public void Init()
     {
+        const string fontName = "Yu Gothic UI";
+        const int fontSize = 28;
+        const int fontThick = 6;
+
         panel = new(400, 200);
-        button = new GUI.Button[9];
-        for (var i = 0; i < 9; i++)
-        {
-            button[i] = new(90, 100, "Yu Gothic UI", 29, 6);
-            button[i].BackColor = Color.FromArgb(39, 39, 44);
-            button[i].EdgeColor = Color.FromArgb(45, 45, 50);
-            button[i].ForeColor = Color.White;
-            button[i].Text = texts[i];
-            panel.ChildControl?.Add(button[i]);
-        }
         panel.Opacity = 0;
+
+        Buttons = new()
+        {
+            { "0", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "1", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "2", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "3", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "4", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "5", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "6", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "7", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "8", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "9", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "+", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "-", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "x", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "/", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "%", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "=", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { ".", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+            { "+/-", new GUI.Button(0, 0, fontName, fontSize, fontThick) },
+        };
+
+        int index = 0;
+        foreach(var item in Buttons)
+        {
+            item.Value.BackColor = Color.FromArgb(39, 39, 44);
+            item.Value.EdgeColor = Color.FromArgb(45, 45, 50);
+            item.Value.ForeColor = Color.White;
+            item.Value.Text = texts[index];
+
+            index++;
+        }
+
+        // 並べる
+        var childs = panel.ChildControl;
+        childs.Add(Buttons["7"]);
+        childs.Add(Buttons["8"]);
+        childs.Add(Buttons["9"]);
+        childs.Add(Buttons["x"]);
+        childs.Add(Buttons["4"]);
+        childs.Add(Buttons["5"]);
+        childs.Add(Buttons["6"]);
+        childs.Add(Buttons["-"]);
+        childs.Add(Buttons["1"]);
+        childs.Add(Buttons["2"]);
+        childs.Add(Buttons["3"]);
+        childs.Add(Buttons["+"]);
+        childs.Add(Buttons["+/-"]);
+        childs.Add(Buttons["0"]);
+        childs.Add(Buttons["."]);
+        childs.Add(Buttons["="]);
     }
 
     public void Update()
     {
-        if (panel == null) return;
+        if (panel == null)
+            return;
 
         DX.GetWindowSize(out int width, out int height);
-        panel.Build(width, height / 2);
+        int x = (width - panel.Size.Width) / 2;
+        int y = height / 2 + Padding;
 
-        int cheight = -1;
-        for (var i = 0; i < panel.ChildControl?.Count(); i++)
-        {
-            if (i % 3 == 0 || i % 6 == 0)
-                cheight++;
-
-            int w = (int)(3 * panel.ChildControl[0].Size.Width + (3 * Padding));
-
-            panel.ChildControl[i].Position = (
-                (int)(i * panel.ChildControl[i].Size.Width + (i * Padding)) - cheight * w + (int)(Padding * 2),
-                (int)(cheight * (panel.ChildControl[i].Size.Height + Padding))
-            );
-
-            panel.ChildControl[i].Build(
-                (int)((width / 3)) - (int)Padding * 2,
-                100
-            );
-        }
-
-        panel.Position = (0, height / 2);
-        panel.Update(0, 0);
+        panel.Position = (x, y);
+        panel.Update(x, y);
+        ReSize();
     }
 
     public void Draw()
     {
+        if (panel == null)
+            return;
+
         panel.Draw();
-        //DX.GetWindowSize(out int width, out int height);
-        //DX.DrawString(0, 0, $"X {width} Y {height}", 0xffffff);
-        //DX.DrawString(0, 20, $"X {panel.Size.Width} Y {panel.Size.Height}", 0xffffff);
     }
 
     public void Finish()
     {
         
+    }
+
+    void ReSize()
+    {
+        if (panel == null)
+            return;
+
+        DX.GetWindowSize(out int width, out int height);
+        panel?.Build(width - Padding * 2, height / 2 - Padding * 2);
+
+        var childs = panel.ChildControl;
+        int heightCount = -1;
+        for (var i = 0; i < childs?.Count(); i++)
+        {
+            // NOTE: 1は誤差
+            int cWidth = (panel.Size.Width / 4) - Padding + 1;
+            int cHeight = (panel.Size.Height / 4) - Padding + 1;
+
+            childs[i].Build(cWidth, cHeight);
+
+            // 座標を計算する
+            if(i % 4 == 0 || i % 8 == 0)
+                heightCount++;
+
+            int addX = ((cWidth * 4) + (4 * Padding)) * heightCount;
+            int x = (cWidth * i) + (i * Padding) - addX;
+            int y = (cHeight * heightCount) + (heightCount * Padding);
+
+            Debug.WriteLine(addX);
+
+            childs[i].Position = (
+                x, y
+            );
+        }
     }
 }
